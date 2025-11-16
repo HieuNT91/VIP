781e4ea5b51b4b5561ebef3212a4cd2c03e9816b
Your Name <hilljun.2000@gmail.com>
2025-11-16 22:26:06 +0000

    fix data source

--- experiments/math/scripts/run_baselines.sh
+++ experiments/math/scripts/run_baselines.sh
@@ -134,7 +134,7 @@ python3 -m train.dapo.main_dapo \
     trainer.val_before_train=False \
     trainer.test_freq=20 \
     trainer.save_freq=67 \
-    trainer.total_epochs=4 \
+    trainer.total_epochs=2 \
     data.shuffle=False \
     trainer.default_local_dir="${CKPTS_DIR}" \
     trainer.resume_mode=auto \
