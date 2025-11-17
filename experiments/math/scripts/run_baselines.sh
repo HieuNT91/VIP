69049b619ea4e31b9d3c2dc9a27adbe1489c8fa3
Your Name <hilljun.2000@gmail.com>
2025-11-17 07:02:39 +0000

    add rollout skip

--- experiments/math/scripts/run_baselines.sh
+++ experiments/math/scripts/run_baselines.sh
@@ -119,6 +119,8 @@ python3 -m train.dapo.main_dapo \
     actor_rollout_ref.rollout.val_kwargs.do_sample=True \
     actor_rollout_ref.rollout.val_kwargs.n=32 \
     actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.skip_rollout=True \
+    actor_rollout_ref.rollout.skip_dump_dir="${HOME_DIR}/tmp/rollout_dump" \
     actor_rollout_ref.ref.fsdp_config.param_offload=${offload} \
     actor_rollout_ref.ref.ulysses_sequence_parallel_size=${sp_size} \
     actor_rollout_ref.actor.fsdp_config.fsdp_size=-1 \
